dfba37fb88e5f556551fdbb0ca35f083da2f70d3
Emile0001 <boteseh@gmail.com>
2023-11-28 21:00:48 +0200

    88-Merge-Sorted-Array solution

--- Easy/88-Merge-Sorted-Array/88-Merge-Sorted-Array.js
+++ Easy/88-Merge-Sorted-Array/88-Merge-Sorted-Array.js
@@ -41,4 +41,37 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function (nums1, m, nums2, n) {};
+var merge = function (nums1, m, nums2, n) {
+    // Start from the end of the merged array
+    let i = m - 1; // index for nums1
+    let j = n - 1; // index for nums2
+    let k = m + n - 1; // index for the merged array
+
+    // Merge in descending order
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // If there are remaining elements in nums2, copy them to nums1
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+};
+
+// Example usage:
+let nums1 = [1, 2, 3, 0, 0, 0];
+let m = 3;
+let nums2 = [2, 5, 6];
+let n = 3;
+
+merge(nums1, m, nums2, n);
+console.log(nums1); // Output: [1, 2, 2, 3, 5, 6]
