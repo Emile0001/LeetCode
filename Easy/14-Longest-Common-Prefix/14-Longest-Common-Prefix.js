15a352984b30d2bd6502b8efdb6d83f0845ecb3c
Emile0001 <boteseh@gmail.com>
2023-12-02 20:21:00 +0200

    14-Longest-Common-Prefix Solution

--- Easy/14-Longest-Common-Prefix/14-Longest-Common-Prefix.js
+++ Easy/14-Longest-Common-Prefix/14-Longest-Common-Prefix.js
@@ -22,4 +22,28 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {};
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 0) {
+        return ""; // Empty array, no common prefix.
+    }
+
+    // Iterate through characters of the first string.
+    for (let i = 0; i < strs[0].length; i++) {
+        const currentChar = strs[0][i];
+
+        // Check the current character against the same position in other strings.
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== currentChar) {
+                // If mismatch or end of any string is reached, return the prefix.
+                return strs[0].substring(0, i);
+            }
+        }
+    }
+
+    // If no mismatch is found, the entire first string is the common prefix.
+    return strs[0];
+};
+
+// Example usage:
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); // Output: "fl"
+console.log(longestCommonPrefix(["dog", "racecar", "car"])); // Output: ""
