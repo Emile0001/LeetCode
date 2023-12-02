ab3a5406971f708d6e98135cef95316bca11d891
Emile0001 <boteseh@gmail.com>
2023-12-02 20:18:51 +0200

    9-Palindrome-Number Solution

--- Easy/9-Palindrome-Number/9-Palindrome-Number.js
+++ Easy/9-Palindrome-Number/9-Palindrome-Number.js
@@ -21,3 +21,26 @@
 // Constraints:
 
 // -231 <= x <= 231 - 1
+var isPalindrome = function (x) {
+    // Special cases: negative numbers and numbers ending with 0 (except 0 itself) are not palindromes.
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
+        return false;
+    }
+
+    let reversed = 0;
+    let original = x;
+
+    // Reverse the number.
+    while (x > 0) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    // Check if the reversed number is equal to the original.
+    return reversed === original;
+};
+
+// Example usage:
+console.log(isPalindrome(121)); // Output: true
+console.log(isPalindrome(-121)); // Output: false
+console.log(isPalindrome(10)); // Output: false
