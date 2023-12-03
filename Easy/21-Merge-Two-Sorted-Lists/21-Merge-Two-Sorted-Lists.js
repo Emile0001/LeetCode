5750391fdf7f07e2abff6354aaeaae98d167b477
Emile0001 <boteseh@gmail.com>
2023-12-03 19:04:23 +0200

    21-Merge-Two-Sorted-Lists solution

--- Easy/21-Merge-Two-Sorted-Lists/21-Merge-Two-Sorted-Lists.js
+++ Easy/21-Merge-Two-Sorted-Lists/21-Merge-Two-Sorted-Lists.js
@@ -35,4 +35,46 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function (list1, list2) {};
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    // Dummy node to simplify code
+    const dummy = new ListNode(0);
+    let current = dummy;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+
+        current = current.next;
+    }
+
+    // If one of the lists is not exhausted, append the remaining nodes
+    if (list1 !== null) {
+        current.next = list1;
+    }
+
+    if (list2 !== null) {
+        current.next = list2;
+    }
+
+    return dummy.next;
+};
+
+// Test cases
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+console.log(mergeTwoLists(list1, list2)); // Output: ListNode { val: 1, next: ListNode { val: 1, next: ListNode { val: 2, next: ListNode { val: 3, next: ListNode { val: 4, next: ListNode { val: 4, next: null } } } } } }
