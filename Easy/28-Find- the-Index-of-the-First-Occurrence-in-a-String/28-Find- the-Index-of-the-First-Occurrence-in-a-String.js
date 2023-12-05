f23d304d524bf790e8bfdcb02d9f72b2d2c84467
Emile0001 <boteseh@gmail.com>
2023-12-05 19:16:38 +0200

    28-Find-the-Index-of-the-First-Occurrence Solution

--- Easy/28-Find- the-Index-of-the-First-Occurrence-in-a-String/28-Find- the-Index-of-the-First-Occurrence-in-a-String.js
+++ Easy/28-Find- the-Index-of-the-First-Occurrence-in-a-String/28-Find- the-Index-of-the-First-Occurrence-in-a-String.js
@@ -21,4 +21,22 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function (haystack, needle) {};
+var strStr = function (haystack, needle) {
+    // Handle edge case where needle is an empty string
+    if (needle === "") {
+        return 0;
+    }
+
+    // Iterate through the haystack string
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        // Check if a substring of haystack matches the needle
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i; // Return the index if a match is found
+        }
+    }
+
+    return -1; // Return -1 if no match is found
+};
+
+console.log(strStr("sadbutsad", "sad")); // Output: 0
+console.log(strStr("leetcode", "leeto")); // Output: -1
