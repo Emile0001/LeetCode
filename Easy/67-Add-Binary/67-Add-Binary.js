3c463c7828617e9581d098159136bcebf40f8d87
Emile0001 <boteseh@gmail.com>
2023-12-05 19:20:18 +0200

    67-Add-Binary Solution

--- Easy/67-Add-Binary/67-Add-Binary.js
+++ Easy/67-Add-Binary/67-Add-Binary.js
@@ -19,4 +19,39 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function (a, b) {};
+var addBinary = function (a, b) {
+    let result = "";
+    let carry = 0;
+
+    // Start from the rightmost bits
+    let i = a.length - 1;
+    let j = b.length - 1;
+
+    // Iterate until both strings are processed
+    while (i >= 0 || j >= 0 || carry > 0) {
+        // Get the current bits from both strings (or 0 if no more bits)
+        const bitA = i >= 0 ? parseInt(a[i]) : 0;
+        const bitB = j >= 0 ? parseInt(b[j]) : 0;
+
+        // Calculate the sum of the bits and the carry
+        const sum = bitA + bitB + carry;
+
+        // Calculate the new carry for the next iteration
+        carry = Math.floor(sum / 2);
+
+        // Calculate the current bit in the result
+        const currentBit = sum % 2;
+
+        // Prepend the current bit to the result
+        result = currentBit + result;
+
+        // Move to the next bits towards the left
+        i--;
+        j--;
+    }
+
+    return result;
+};
+
+console.log(addBinary("11", "1")); // Output: "100"
+console.log(addBinary("1010", "1011")); // Output: "10101"
