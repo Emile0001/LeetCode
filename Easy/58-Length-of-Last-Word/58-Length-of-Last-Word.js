a719c0996f13f7ddabe6f92568848f4c6dc4766f
Emile0001 <boteseh@gmail.com>
2023-12-05 19:19:15 +0200

    58-Length-of-Last-Word Solution

--- Easy/58-Length-of-Last-Word/58-Length-of-Last-Word.js
+++ Easy/58-Length-of-Last-Word/58-Length-of-Last-Word.js
@@ -30,4 +30,50 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function (s) {};
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal
+// substring
+//  consisting of non-space characters only.
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+    // Trim any leading or trailing spaces
+    s = s.trim();
+
+    // Find the last space in the trimmed string
+    const lastSpaceIndex = s.lastIndexOf(" ");
+
+    // If there is no space, the entire string is the last word
+    // Otherwise, return the length of the substring after the last space
+    return lastSpaceIndex === -1 ? s.length : s.length - lastSpaceIndex - 1;
+};
+
+console.log(lengthOfLastWord("Hello World")); // Output: 5
+console.log(lengthOfLastWord("   fly me   to   the moon  ")); // Output: 4
+console.log(lengthOfLastWord("luffy is still joyboy")); // Output: 6
