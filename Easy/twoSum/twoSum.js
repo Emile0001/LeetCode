fd3293f8a08bdfd0aafcbebd193d242077db3539
Emile0001 <boteseh@gmail.com>
2023-12-15 14:21:44 +0200

    twoSum 2nd solution

--- Easy/twoSum/twoSum.js
+++ Easy/twoSum/twoSum.js
@@ -41,4 +41,18 @@ let twoSum = function (nums, target) {
         }
     }
 };
-console.log(twoSum([3, 2, 4], 5));
+console.log(twoSum([3, 2, 4], 6));
+
+var twoSum2nd = function (nums, target) {
+    for (let i = 0; i <= nums.length; i++) {
+        for (let j = 1; j <= nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                let number = [];
+                number.push(i, j);
+                return console.log(number);
+            }
+        }
+    }
+};
+
+twoSum2nd([2, 7, 11, 15], 9);
